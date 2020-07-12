8233d9489906e1effb8c60aebedfb5ed9266b629
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-07-11 18:35:03 -0700

    working with strings

--- app.py
+++ app.py
@@ -25,3 +25,51 @@ character_name = "Mike"
 
 print("He really liked the name " + character_name + ", ")
 print("but didn't like being " + character_age + ".")
+
+# new line in a string
+# \ is an escape character
+print("Giraffe \nAcademy")
+
+# string variable
+phrase = "Hello World"
+print(phrase)
+
+# concatonating a string
+print(phrase + " my name is Loralie")
+
+# built in python functions
+# put to upper case
+print(phrase.upper())
+
+# built in python functions
+# puts to upper case then checks for upper case and returns boolean
+print(phrase.upper().isupper())
+
+# built in python functions
+# checking the length of a string
+print(len(phrase))
+
+# built in python functions
+# finding a character by index on a string to get the character returned
+print(phrase[3])
+
+# built in python functions
+# finding the index of a character and getting the index returned
+print(phrase.index("W"))
+
+# built in python functions
+# replacing a word in a string
+print(phrase.replace("Hello", "Welcome"))
+
+
+
+
+
+
+
+
+
+
+
+
+
