eb14d66b5ee4f583d0a9b6d655cb0402370b6b49
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-07-11 19:03:28 -0700

    getting input from users

--- app.py
+++ app.py
@@ -138,6 +138,16 @@ print(ceil(8.6))
 print(sqrt(36))
 
 
+# getting input from users
+input("Enter your name: ")
+
+# storing input to a variable
+name = input("Enter your name: ")
+user_age = input("Enter your age: ")
+print("Hello " + name + "! You are " + user_age)
+
+
+
 
 
 
