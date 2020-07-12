ff9606b56d79b6bb157ae83704c78b09e3bdcd5b
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-07-12 16:07:28 -0700

    list functions

--- app.py
+++ app.py
@@ -154,7 +154,7 @@ result = float(num1) + float(num2)
 print(result)
 
 # lists in python are arrays in javascript
-friends = ["Jess", "Hope", "Lexi", "Tattoo Girl"]
+friends = ["Jess", "Jess", "Jess", "Hope", "Lexi", "Tattoo Girl"]
 
 # lists can hold stings numbers and booleans
 friends2 = ["Jess", 1, True]
@@ -179,6 +179,57 @@ print(friends[1:3])
 friends[1] = "Hopey"
 print(friends)
 
+# LIST FUNCTIONS
+lucky_numbers = [4, 8, 15, 42, 16, 23]
+
+# extending a list
+# this will combine 2 lists into 1
+# friends.extend(lucky_numbers)
+# print(friends)
+
+# adding an item to the end of a list
+friends.append("Dad")
+print(friends)
+
+# inserting an item to a list
+# takes 2 parameters index to insert item and item
+friends.insert(1, "Kelly")
+print(friends)
+
+# remove item from list
+friends.remove("Kelly")
+
+print(friends)
+
+# deleting last item in list
+friends.pop()
+print(friends)
+
+# finding the index of an ittem in a list
+print(friends.index("Jess"))
+
+# how many multiples of a item are in a list
+print(friends.count("Jess"))
+
+# sorting a list from acending to decending or alphabetically
+friends.sort()
+print(friends)
+
+# sorting a list from acending to decending or alphabetically
+lucky_numbers.sort()
+print(lucky_numbers)
+
+# reversing the order of a list
+lucky_numbers.reverse()
+print(lucky_numbers)
+
+# copying a list
+new_friends = friends.copy()
+print(new_friends)
+
+# to delete an entire list
+friends.clear()
+print(friends)
 
 
 
