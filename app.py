0d8530e1c352271a93a9fb9405ed58118cfb57fd
ittyBittyBytes <loralie.flint0128@gmail.com>
2020-07-11 18:18:42 -0700

    basic data types stings, numbers, booleans

--- app.py
+++ app.py
@@ -2,3 +2,26 @@ print("   /|")
 print("  / |")
 print(" /  |")
 print("/___|")
+
+# variable with a string
+character_name = "John"
+
+# variable with a number as a string
+character_age = "35"
+
+# variable with a number
+age = 35
+
+# variable with number in decimals
+age_in_decimal = 35.222
+
+# variable with boolean
+is_male = True
+
+print("There once was a man named " + character_name + ", ")
+print("He was " + character_age + " years old.")
+
+character_name = "Mike"
+
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
